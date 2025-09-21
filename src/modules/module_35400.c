e12b9dcdd45f419a79069fe9235f652d59d4b38d
Coontzy1 <coontz.austin@gmail.com>
2025-09-21 15:36:38 -0400

    Renaming 13150/18250 to 35300/35400

--- src/modules/module_35400.c
+++ src/modules/module_35400.c
@@ -18,12 +18,13 @@ static const u32   DGST_POS3      = 3;
 static const u32   DGST_SIZE      = DGST_SIZE_4_4;
 static const u32   HASH_CATEGORY  = HASH_CATEGORY_NETWORK_PROTOCOL;
 static const char *HASH_NAME      = "Kerberos 5, etype 23, AS-REP (NT)";
-static const u64   KERN_TYPE      = 18250;
+static const u64   KERN_TYPE      = 35400;
 static const u32   OPTI_TYPE      = OPTI_TYPE_ZERO_BYTE
                                   | OPTI_TYPE_NOT_ITERATED;
 static const u64   OPTS_TYPE      = OPTS_TYPE_STOCK_MODULE
                                   | OPTS_TYPE_PT_GENERATE_LE
-                                  | OPTS_TYPE_ST_HEX;
+                                  | OPTS_TYPE_ST_HEX
+                                  | OPTS_TYPE_AUTODETECT_DISABLE;
 static const u32   SALT_TYPE      = SALT_TYPE_EMBEDDED;
 static const char *ST_PASS        = "b4b9b02e6f09a9bd760f388b67351e2b";
 static const char *ST_HASH        = "$krb5asrep$23$user@domain.com:3e156ada591263b8aab0965f5aebd837$007497cb51b6c8116d6407a782ea0e1c5402b17db7afa6b05a6d30ed164a9933c754d720e279c6c573679bd27128fe77e5fea1f72334c1193c8ff0b370fadc6368bf2d49bbfdba4c5dccab95e8c8ebfdc75f438a0797dbfb2f8a1a5f4c423f9bfc1fea483342a11bd56a216f4d5158ccc4b224b52894fadfba3957dfe4b6b8f5f9f9fe422811a314768673e0c924340b8ccb84775ce9defaa3baa0910b676ad0036d13032b0dd94e3b13903cc738a7b6d00b0b3c210d1f972a6c7cae9bd3c959acf7565be528fc179118f28c679f6deeee1456f0781eb8154e18e49cb27b64bf74cd7112a0ebae2102ac";
@@ -268,14 +269,12 @@ int module_hash_decode (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSE
   /* this is needed for hmac_md5 */
   *edata_ptr++ = 0x80;
 
-  /* use checksum as “salt” so the backends can filter */
   salt->salt_buf[0] = krb5asrep->checksum[0];
   salt->salt_buf[1] = krb5asrep->checksum[1];
   salt->salt_buf[2] = krb5asrep->checksum[2];
   salt->salt_buf[3] = krb5asrep->checksum[3];
   salt->salt_len    = 16;
 
-  /* set digest to checksum; device comp will recompute and compare */
   digest[0] = krb5asrep->checksum[0];
   digest[1] = krb5asrep->checksum[1];
   digest[2] = krb5asrep->checksum[2];
